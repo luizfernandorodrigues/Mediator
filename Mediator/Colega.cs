bfbabbea4a420050190ffe6b01bf5a292b1e32e0
Luiz Fernando <luizfernando_rodrigues12@hotmail.com>
2020-04-09 19:15:40 -0300

    Add project files.

---
+++ Mediator/Colega.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediator
+{
+    public abstract class Colega
+    {
+        protected Mediador mediador;
+
+        public Colega(Mediador mediador)
+        {
+            this.mediador = mediador;
+        }
+    }
+}
